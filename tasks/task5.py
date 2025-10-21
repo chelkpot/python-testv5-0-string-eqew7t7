128d5239cd7d5309c73dde4993d82efec1a03ec8
eqew7t7 <awqqurgis@gmail.com>
2025-10-21 22:23:18 +0500

    решение задач

--- tasks/task5.py
+++ tasks/task5.py
@@ -2,7 +2,11 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
+    a, b, c = input().split()
+    print('ASCII', a, ord(a))
+    print('ASCII', b, ord(b))
+    print('ASCII', c, ord(c))
+
    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
